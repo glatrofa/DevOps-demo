bf13c761b0a263b607b8c2339c205b5217ebdb26
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-20 19:34:49 +0100

    Modify folders path

--- ps-scripts/deploy-client-image.ps1
+++ ps-scripts/deploy-client-image.ps1
@@ -1,4 +1,3 @@
-# deploy client image to docker hub with v1.0
-docker build -t client:1.0 .
+docker build -t client:1.0 ../client/
 docker tag client:1.0 glatrofa/dev-ops-demo:client-1.0
 docker push glatrofa/dev-ops-demo:client-1.0
--- ps-scripts/deploy-server-image.ps1
+++ ps-scripts/deploy-server-image.ps1
@@ -1,4 +1,3 @@
-# deploy server image to docker hub with v1.0
-docker build -t server:1.0 .
+docker build -t server:1.0 ../server/
 docker tag server:1.0 glatrofa/dev-ops-demo:server-1.0
 docker push glatrofa/dev-ops-demo:server-1.0
