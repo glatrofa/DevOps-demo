ee4a693e8cb0e7c19eb6be14036830790c24b8cc
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-20 19:34:29 +0100

    Delete jenkins

--- ps-scripts/launch-dev-namespace.ps1
+++ ps-scripts/launch-dev-namespace.ps1
@@ -1,6 +1,4 @@
-# apply all yaml file for the dev namespace
 kubectl apply -f ./yaml/dev/deployments/client-deployment.yml
-kubectl apply -f ./yaml/dev/deployments/jenkins-deployment.yml
 kubectl apply -f ./yaml/dev/deployments/mysql-deployment.yml
 kubectl apply -f ./yaml/dev/deployments/phpmyadmin-deployment.yml
 kubectl apply -f ./yaml/dev/deployments/server-deployment.yml
