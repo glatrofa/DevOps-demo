ce22cb0e82dc8b7644d5134980bb9a096089507f
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-20 19:34:13 +0100

    Update internatl db adress

--- server/src/index.php
+++ server/src/index.php
@@ -7,7 +7,7 @@ phpinfo();
 phpinfo(INFO_MODULES);
 */
 echo "<h1>Test mysql db on k8s</h1>";
-$mysqli = new mysqli("10.107.82.215", "root", "code-architects", "test", 3306);
+$mysqli = new mysqli("10.108.69.30", "root", "code-architects", "test", 3306);
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
