3ceb47d63cc9c880c377ca89a1fd94143589ab63
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-23 09:57:15 +0100

    Delete stop dev namespace.ps1

--- ps-scripts/stop-dev-namespace.ps1
+++
@@ -1,4 +0,0 @@
-kubectl delete -f ./yaml/dev/deployments/client-deployment.yml
-kubectl delete -f ./yaml/dev/deployments/mysql-deployment.yml
-kubectl delete -f ./yaml/dev/deployments/phpmyadmin-deployment.yml
-kubectl delete -f ./yaml/dev/deployments/server-deployment.yml
