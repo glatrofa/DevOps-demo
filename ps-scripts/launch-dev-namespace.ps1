1d7dcf10e084b3e776a601d86c41b58b3acdecfb
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-23 09:57:02 +0100

    Delete launch dev namespace.ps1

--- ps-scripts/launch-dev-namespace.ps1
+++
@@ -1,4 +0,0 @@
-kubectl apply -f ./yaml/dev/deployments/client-deployment.yml
-kubectl apply -f ./yaml/dev/deployments/mysql-deployment.yml
-kubectl apply -f ./yaml/dev/deployments/phpmyadmin-deployment.yml
-kubectl apply -f ./yaml/dev/deployments/server-deployment.yml
