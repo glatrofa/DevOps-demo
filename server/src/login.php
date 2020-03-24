3348d5de6ac6ccb5a92a9a6ea4e1275670d5d7e2
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-24 13:51:43 +0100

    Fix the ajax response with json object

--- server/src/login.php
+++ server/src/login.php
@@ -1,8 +1,8 @@
 <?php
 
+header('Access-Control-Allow-Origin: *'); // resolve the "Cross-Origin Read Blocking (CORB)" error
 include 'db-info.php';
 
-echo "<html>";
 $username = mysqli_real_escape_string($connection, $_POST["username"]);
 $password = mysqli_real_escape_string($connection, $_POST["password"]);
 $password_criptata = md5($password);
@@ -11,26 +11,24 @@ $query = "SELECT username, email FROM user WHERE username = '".$username."' AND 
 $result = mysqli_query($connection, $query) or die("Access failed");
 $rowsNumber = mysqli_num_rows($result);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-//echo "rowsNumber=".$rowsNumber;
+$response = array();
 if($rowsNumber != 0){
-    $response = array();
     $response[0] = array('username' => $row["username"], 'email' => $row["email"]);
-    //$json_data = json_encode($response);
-    //file_put_contents('user_info.json', $json_data);
-    //echo $json_data;     
-    echo utf8_decode(json_encode($response))."<br>";
-    echo "<head><meta http-equiv='refresh' content='2; URL=http://localhost:30002/index.html?username=".$row["username"]."&email=".$row["email"]."' /></head><body>";
-    echo "<strong>Accesso eseguito!</strong> <br> Verrai reindirizzato a breve...";
+    //$response[0] = $row["username"];
+    //$response[1] = $row["email"];     
 }
 else{
-    $response = array();
     $response[0] = array('username' => null, 'email' => null);
-    //$json_data = json_encode($response);
-    //echo $json_data;  
-    echo utf8_decode(json_encode($response))."<br>";
-    echo "<head><meta http-equiv='refresh' content='2; URL=http://localhost:30002/html/register.html' /></head><body>";
-    echo "<strong>Utente non registrato!</strong> <br> Verrai reindirizzato a breve...";
+    //$response[0] = null;
+    //$response[1] = null; 
 }
-echo "</body></html>";
+$json_data = json_encode($response);
+echo $json_data; 
+
+/* 
+doesn't work:
+$response -> username = $row["username"];
+$response -> email = $row["email"]; 
+*/
 
 ?>
