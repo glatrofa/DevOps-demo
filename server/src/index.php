00f2981213a63be3e8a3ee082bb27bc8cf50187d
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-03-24 13:51:05 +0100

    Delete the comments

--- server/src/index.php
+++ server/src/index.php
@@ -4,11 +4,4 @@ include 'db-info.php';
 
 echo $connection->host_info . "\n";
 
-/*
-$res = $connection->query("SELECT username, email FROM user");
-while ($row = $res->fetch_assoc()) {
-    echo "<br> Username: " . $row['username'] . "\n Email: " . $row['email'] . "\n";
-}
-*/
-
 ?>
