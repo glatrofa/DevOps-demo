948f8a13fc6373d9278b4ed26ef842210ba5ddda
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-04-01 16:48:35 +0200

    Update version image

--- ps-scripts/deploy-client-image.ps1
+++ ps-scripts/deploy-client-image.ps1
@@ -1,3 +1,3 @@
-docker build -t client:1.0 ../client/
-docker tag client:1.0 glatrofa/dev-ops-demo:client-1.0
-docker push glatrofa/dev-ops-demo:client-1.0
+docker build -t client:1.1 ../client/
+docker tag client:1.1 glatrofa/dev-ops-demo:client-1.1
+docker push glatrofa/dev-ops-demo:client-1.1
--- ps-scripts/deploy-server-image.ps1
+++ ps-scripts/deploy-server-image.ps1
@@ -1,3 +1,3 @@
-docker build -t server:1.0 ../server/
-docker tag server:1.0 glatrofa/dev-ops-demo:server-1.0
-docker push glatrofa/dev-ops-demo:server-1.0
+docker build -t server:1.1 ../server/
+docker tag server:1.1 glatrofa/dev-ops-demo:server-1.1
+docker push glatrofa/dev-ops-demo:server-1.1
