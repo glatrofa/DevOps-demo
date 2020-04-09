34f6e4b60fdb5e98f28d5220cb6ffe1a5fd7bffe
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-04-09 11:41:50 +0200

    Use let instead of var and delete document.ready function

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,10 +1,8 @@
 import { API } from "./config/config.js";
 
-$(document).ready(function () {
-  document.getElementById('register').onclick = function () {
-    location.href = "./html/register.html";
-  };
-});
+document.getElementById('register').onclick = function () {
+  location.href = "./html/register.html";
+};
 
 $(function showUserInfo() {
     $('form').on('submit', function (e) {
@@ -18,19 +16,19 @@ $(function showUserInfo() {
         success: function (data) {
             console.log(data);
             if(data[0].username != null && data[0].email != null){
-              var string = '<div class="alert alert-success" role="alert">'+'<h5>Login done!</h5>'+'Username: '+ data[0].username +'<br>Email: '+ data[0].email+'</div>';
+              let string = '<div class="alert alert-success" role="alert">'+'<h5>Login done!</h5>'+'Username: '+ data[0].username +'<br>Email: '+ data[0].email+'</div>';
               document.getElementById('user-info').innerHTML = string;
             }
             else{
-              var string = '<div class="alert alert-warning" role="alert">'+'<h5>Your account does not exist</h5>'+'</div>';
-              var register_button = "<div class='centered button-padding'><button class='btn btn-light' id='register'>Sign up</button></div>";
+              let string = '<div class="alert alert-warning" role="alert">'+'<h5>Your account does not exist</h5>'+'</div>';
+              let register_button = "<div class='centered button-padding'><button class='btn btn-light' id='register'>Sign up</button></div>";
               document.getElementById('user-info').innerHTML = string + register_button;
             }               
         },
         error: function (data) {
           console.log(data);
           alert('Error');
-          var string = '<div class="alert alert-danger" role="alert">'+'<h3>An error has occurred</h3>'+'</div>';
+          let string = '<div class="alert alert-danger" role="alert">'+'<h3>An error has occurred</h3>'+'</div>';
           document.getElementById('user-info').innerHTML = string;
         }
       });
