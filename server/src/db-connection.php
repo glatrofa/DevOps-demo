9d59027f259ef1088f1a472a4695eff3e12b2db7
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-04-09 10:29:06 +0200

    Add default calues for connection parameters in case of env variables not defined

--- server/src/db-connection.php
+++ server/src/db-connection.php
@@ -1,10 +1,20 @@
 <?php
 
-$dbPassword = getenv('DB_PASSWORD');
 $dbUserName = getenv('DB_USER_NAME');
+if(!isset($dbUserName))
+    $dbUserName = "root";
+$dbPassword = getenv('DB_PASSWORD');
+if(!isset($dbPassword))
+    $dbPassword = "code-architects";
 $dbServer = getenv('SERVER_ADDRESS');
+if(!isset($dbServer))
+    $dbServer = "10.105.26.205";
 $dbName = getenv('DB_NAME');
-$port = 3306;
+if(!isset($dbName))
+    $dbName = "user";
+$port = getenv('PORT_NUMBER');
+if(!isset($port))
+    $port = "3306";
 
 $connection = new mysqli($dbServer, $dbUserName, $dbPassword, $dbName, $port);
 if($connection->connect_errno){
