7659277bfa6c5fbc186f17f258f401c472dc203f
Giuseppe Latrofa <latrofagpp@gmail.com>
2020-04-09 11:41:28 +0200

    Use let instead of var

--- client/src/js/register.js
+++ client/src/js/register.js
@@ -11,18 +11,18 @@ $(function registerUser() {
         dataType: 'json',
         success: function (data) {
             console.log(data);
-            var url = '../index.html';
+            let url = '../index.html';
             switch (data[0]) {
                 case 0:
-                    var string = '<div class="alert alert-success" role="alert">'+'<h5>Registration successful!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
+                    let string = '<div class="alert alert-success" role="alert">'+'<h5>Registration successful!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
                     break;
                 case 1:
-                    var string = '<div class="alert alert-warning" role="alert">'+'<h5>Email already used!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
-                    var url = './register.html';
+                    let string = '<div class="alert alert-warning" role="alert">'+'<h5>Email already used!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
+                    url = './register.html';
                     break;
                 case 2:      
-                    var string = '<div class="alert alert-warning" role="alert">'+'<h5>Username already used!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
-                    var url = './register.html';
+                    let string = '<div class="alert alert-warning" role="alert">'+'<h5>Username already used!</h5>'+'<p>You will be redirected in 2 seconds...</p></div>';
+                    url = './register.html';
                     break;              
             }         
             setTimeout(function () {
